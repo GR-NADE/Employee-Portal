8cb8b65dd6a9828ff979ce8a0433d5ecd742484b
GR-NADE <ikenuavictor@gmail.com>
2025-03-20 20:26:58 +0100

    Initial commit

--- obj/Debug/net9.0/CreateAndSuccess.AssemblyInfo.cs
+++ obj/Debug/net9.0/CreateAndSuccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateAndSuccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44247081aa2277142f6fb337f61cca1008150975")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateAndSuccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateAndSuccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
